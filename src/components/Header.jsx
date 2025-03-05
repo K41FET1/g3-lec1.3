e986e46bf08282465fed8220d2862adf7c6a2c19
DavitKutsia <ittzarctic33@gmail.com>
2025-03-05 20:41:04 +0400

    use tailwind css instead of the normal css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import DarkLight from './DarkLight'
-import '../index.css'
+import React from 'react';
+import DarkLight from './DarkLight';
 
 export default function Header() {
   return (
     <div>
-      <header>
-        <div className='titleDiv'>
-          <div className='logoDiv'><img src="../assets/Accessibility.svg" alt="" /></div>
-          <p className='title'>Accessibility</p>  
+      <header className="w-full h-[10dvh] bg-transparent px-[150px] py-[100px] flex items-center justify-end">
+        <div className="hidden items-center gap-5">
+          <div className="w-15 h-15 bg-purple-200 rounded-lg flex items-center justify-center">
+            <img src="../assets/Accessibility.svg" alt="Accessibility Logo" />
+          </div>
+          <p className="text-lg font-semibold text-gray-900">Accessibility</p>
         </div>
-        <DarkLight/>
+        <DarkLight />
       </header>
     </div>
-  )
+  );
 }
